a96d33942eff6a5c9fb7e4be94fd78395564c837
jaideep jat <jaideepjat23@gmail.com>
2025-08-25 15:18:08 +0530

    switch case

---
+++ com.ok.javainonevedio/SwtichStatment/Fruit.java
@@ -0,0 +1,29 @@
+package SwtichStatment;
+
+import java.util.Scanner;
+
+public class Fruit {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("enter the fruite name : ");
+        String fruite = in.next();
+
+        switch (fruite) {
+            case "mango":
+                System.out.println("king of fruits");
+                break;
+            case "banana":
+                 System.out.println("good for weight gain");
+                 break;
+            case "kiwi":
+            System.out.println("doof for health ");
+                 break;
+                 case "apple":
+                 System.out.println("one apple a day keeps a dr away ");
+            default:
+            System.out.println("please enter the valid fruite");
+
+        }
+
+    }
+}
