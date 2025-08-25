bf4012a666f57b4dc8019d221490282e23e72641
jaideep jat <jaideepjat23@gmail.com>
2025-08-25 15:46:36 +0530

    new method or modified method of switch case

--- com.ok.javainonevedio/SwtichStatment/Frute.java
+++ com.ok.javainonevedio/SwtichStatment/Frute.java
@@ -14,5 +14,23 @@ public class Frute {
         case "kiwi" ->System.out.println("good for health");
     default -> System.out.println("this statement is false");
     }
+
+     System.out.print("enter the number : ");
+    int days = in.nextInt();
+   
+    
+    switch (days){
+    //     case 1 -> System.out.println("monday");
+    //     case 2 -> System.out.println("tuesday");
+    //     case 3 -> System.out.println("thursday");
+    //     case 4 -> System.out.println("friday");
+    //     case 5 -> System.out.println("wednesday");
+    //     case 6 -> System.out.println("saturday");
+    //     case 7 -> System.out.println("sunday");
+    //     default -> System.out.println("the statement is wrong ");
+    
+    case 1, 2, 3, 4, 5 -> System.out.println("weekday");
+    case 6, 7 -> System.out.println("weekend");
 }
+    }
 }
