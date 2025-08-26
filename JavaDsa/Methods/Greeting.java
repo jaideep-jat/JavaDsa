cad5239bd714c8d507479d6fbd875ee051985017
jaideep jat <jaideepjat23@gmail.com>
2025-08-26 17:45:34 +0530

    string greeting using methods

---
+++ JavaDsa/Methods/Greeting.java
@@ -0,0 +1,10 @@
+package Methods;
+
+public class Greeting {
+    public static void main(String[] args) {
+        Greeting();
+    }
+    static void Greeting(){
+        System.out.println("hello world");
+    }
+}
