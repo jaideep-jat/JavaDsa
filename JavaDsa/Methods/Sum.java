5817eb9989410231d3fc04d9bc778e40dad0e0d1
jaideep jat <jaideepjat23@gmail.com>
2025-08-26 18:47:46 +0530

    sum using attributes method

--- JavaDsa/Methods/Sum.java
+++ JavaDsa/Methods/Sum.java
@@ -1,10 +1,7 @@
 package Methods;
 import java.util.Scanner;
 public class Sum {
-    public static void main(String[] args) {
-        int ans = sum2();
-        System.out.println(ans);
-    }
+
     // static void sum(){
     //   Scanner sc = new Scanner(System.in);
     //   System.out.print("enter number 1: ");
@@ -14,14 +11,30 @@ public class Sum {
     // int sum = a+b;
     // System.out.println("the sum is = " + sum );
 
-    static int sum2(){
-      Scanner sc = new Scanner(System.in);
-      System.out.print("enter number 1: ");
-      int a = sc.nextInt();
-      System.out.print("enter numbver 2: ");
-      int b = sc.nextInt();
-       int sum = a+b;
-      return sum;
+ // THIS METHOD IS FOR RETURNING THE VALUE 
+    
+ //  public static void main(String[] args) {
+     //         int ans = sum2();
+     //         System.out.println(ans);
+     //     }  
+    // static int sum2(){
+    //   Scanner sc = new Scanner(System.in);
+    //   System.out.print("enter number 1: ");
+    //   int a = sc.nextInt();
+    //   System.out.print("enter numbver 2: ");
+    //   int b = sc.nextInt();
+    //    int sum = a+b;
+    //     return sum; // when we use return means everything is over nothing will execute after that
+    
+ // pass the number of value when are calling the method in main()   
+
+    public static void main(String[] args) {
+        int ans = sum3(20,40);
+        System.out.println(ans);
     }
+      static int sum3(int a, int b){
+      int sum = a+b;
+      return sum;
+      }
     }
 
