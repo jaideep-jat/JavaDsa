46375f94435f0c7d3c0e6072dec5096cf64e6917
jaideep jat <jaideepjat23@gmail.com>
2025-08-26 17:41:33 +0530

    returning sum using methods

--- JavaDsa/Methods/Sum.java
+++ JavaDsa/Methods/Sum.java
@@ -2,15 +2,26 @@ package Methods;
 import java.util.Scanner;
 public class Sum {
     public static void main(String[] args) {
-        sum();
+        int ans = sum2();
+        System.out.println(ans);
     }
-    static void sum(){
+    // static void sum(){
+    //   Scanner sc = new Scanner(System.in);
+    //   System.out.print("enter number 1: ");
+    //   int a = sc.nextInt();
+    //   System.out.print("enter numbver 2: ");
+    //   int b = sc.nextInt();
+    // int sum = a+b;
+    // System.out.println("the sum is = " + sum );
+
+    static int sum2(){
       Scanner sc = new Scanner(System.in);
       System.out.print("enter number 1: ");
       int a = sc.nextInt();
       System.out.print("enter numbver 2: ");
       int b = sc.nextInt();
-    int sum = a+b;
-    System.out.println("the sum is = " + sum );
+       int sum = a+b;
+      return sum;
     }
-}
+    }
+
