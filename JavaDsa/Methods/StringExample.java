d482f7021e0d4099b17b14ad1dec7075c764eda3
jaideep jat <jaideepjat23@gmail.com>
2025-08-27 12:13:45 +0530

    string example using methods

---
+++ JavaDsa/Methods/StringExample.java
@@ -0,0 +1,21 @@
+package Methods;
+
+public class StringExample {
+    // public static void main(String[] args) {
+    //     String message = greet();
+    //     System.out.println(message);
+    // }
+    // static String greet(){
+    //     String greeting = " hey jaideep how are you ";
+    //     return greeting;
+    // }
+
+     public static void main(String[] args) {
+        String personalized = greet("jaideep");
+        System.out.println(personalized);
+    }
+    static String greet(String name){
+        String greeting = "hello " + name;
+        return greeting;
+    }
+}
---
+++ JavaDsa/Methods/Swap.java
@@ -0,0 +1,14 @@
+package Methods;
+
+public class Swap {
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 23;
+
+        //Swap number code
+        int temp = a;
+        a = b;
+        b = temp;
+        System.out.println("a is: " + a + "\n"  + "b is: " + b);
+    }
+}
