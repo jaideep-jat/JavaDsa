90f22ef9bb2afd15c0ae555fdcbd4f6f0d987447
jaideep jat <jaideepjat23@gmail.com>
2025-08-28 23:45:15 +0530

    array 1

---
+++ JavaDsa/practice/Array.java
@@ -0,0 +1,11 @@
+package practice;
+
+public class array {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3};   // array creation
+        for(int i : arr) {       // print array elements
+            System.out.print(i + " ");
+        }
+    }
+}
+
