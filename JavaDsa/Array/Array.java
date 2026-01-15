69ecd2b87a13e0bb0eede36c95cb0047d9ecba44
jaideep jat <jaideepjat23@gmail.com>
2026-01-16 00:12:52 +0530

    array

--- JavaDsa/Array/Array.java
+++ JavaDsa/Array/Array.java
@@ -1,13 +1,35 @@
 package Array;
+ import java.util.Scanner;
+public class Array{
+  public static void main(String[] args) {
+    // int marks[] = new int [3];
+    // marks[0] = 98; // english
+    // marks[1] = 92; // physics
+    // marks[2] = 95; // chemistry
 
-public class Array {
-    public static void main(String[] args) {
-        int[] a = new int[5];
-        a[3] =44;  
- 
-        for(int i=0; i<5; i++){
-    System.out.println(a[i]);
+    // for(int i =0; i<3; i++){
+    //   System.out.print(marks[i] + " ");
+    // }
+    
+
+    // take array size as a input
+     
+    Scanner sc = new Scanner(System.in);
+    System.out.print( " enter the size of array : ");
+    int n = sc.nextInt();
+    int marks[] = new int [n];
+    System.out.print("enter the marks : ");
+      
+    for(int i=0; i<n; i++){
+      
+      marks[i] = sc.nextInt();
+        }
+
+   for(int i =0; i<n; i++){
+    System.out.print(marks[i] + " ");
+   }
   }
+}
 
-}
-}
+
+
