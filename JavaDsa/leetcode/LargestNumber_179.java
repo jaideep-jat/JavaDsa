1d781a53ba3cf3adbc708a6e529082e0a6ca8bba
jaideep jat <jaideepjat23@gmail.com>
2026-01-17 23:54:37 +0530

    largest number in an array

---
+++ JavaDsa/leetcode/LargestNumber_179.java
@@ -0,0 +1,9 @@
+package leetcode;
+public class LargestNumber_179 {
+   public static void main(String[] args) {
+    int num[] = {10,2};
+    for(int i=num.length -1; i>=0; i--){
+        System.out.print(num[i]);
+    }
+   }
+}
